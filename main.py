384beb293a545edb649a336c3335c7858ddd1821
xv9c <88051551+Dtrox4@users.noreply.github.com>
2025-04-29 14:35:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ intents.message_content = True
 intents.voice_states = True      # Enable voice state intents
 
 # Create a bot instance with command prefix '!' and the specified intents
-bot = commands.Bot(command_prefix='.', intents=intents)
+bot = commands.Bot(command_prefix=".", intents=discord.Intents.all())
 
 message_id_to_listen = None
 
